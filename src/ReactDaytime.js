c5094f36b8685aa4ea9d7559fd2ef1dbb71447f2
Taha ZIADEH <taha.ziadeh@navybits.com>
2020-01-17 15:50:22 +0200

    ability to accept hour divider

--- src/ReactDaytime.js
+++ src/ReactDaytime.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import uuid from 'uuid4';
+import React from "react";
+import PropTypes from "prop-types";
+import uuid from "uuid4";
 
-import DayTimeCanvas from './canvas';
-import { WIDTH, HEIGHT } from './constants';
+import DayTimeCanvas from "./canvas";
+import { WIDTH, HEIGHT } from "./constants";
 
 class ReactDaytime extends React.Component {
-    constructor(props) {
-        super(props);
-        this.canvasId = 'react-daytime-' + uuid();
-    }
-    componentWillMount() {
-        this.canvas = new DayTimeCanvas(this.props.onChange, this.props.defaultValue, this.props.theme);
-    }
-    componentDidMount() {
-        this.canvas.render(this.canvasId);
-    }
-	render () {
+	constructor(props) {
+		super(props);
+        this.canvasId = "react-daytime-" + uuid();
+        // Ability to divide the hour
+		this.hourDivider = props.hourDivider || 1;
+	}
+	componentWillMount() {
+		this.canvas = new DayTimeCanvas(
+			this.props.onChange,
+			this.props.defaultValue,
+			this.props.theme,
+			this.hourDivider
+		);
+	}
+	componentDidMount() {
+		this.canvas.render(this.canvasId);
+	}
+	render() {
 		return <canvas id={this.canvasId} width={WIDTH} height={HEIGHT} />;
 	}
 }
 ReactDaytime.propTypes = {
-    defaultValue: PropTypes.object,
-    onChange: PropTypes.func,
-    theme: PropTypes.object
+	defaultValue: PropTypes.object,
+	onChange: PropTypes.func,
+	theme: PropTypes.object
 };
 
 export default ReactDaytime;
